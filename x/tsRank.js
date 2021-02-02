51a8397dbcf1963d80a0e2003683416d149ddfe7
nfour <novus.nfour@gmail.com>
2021-02-02 23:22:36 +1000

    bump

--- x/tsRank.js
+++ x/tsRank.js
@@ -6,97 +6,104 @@ const lodash_1 = require("lodash");
 const path_1 = require("path");
 const yargs_1 = require("yargs");
 const MicroMatch = require("micromatch");
-const c = console;
+const clc = require("cli-color");
 const cwd = process.cwd();
-const typeChars = 30; // TODO: automate
-const durChars = 7; // TODO: automate
-let { logCount, traceFile, typesFile, pattern = '**/*' } = yargs_1.default(process.argv.slice(2))
+const TYPE_CHARS_LEN = 30; // TODO: automate
+let { FILE_LIMIT, FILE_TYPE_LIMIT, TRACE_FILE, TYPES_FILE, PATTERN = '**/*' } = yargs_1.default(process.argv.slice(2))
     .usage('ts-rank')
     .options({
-    logCount: { type: 'number', default: 10 },
-    traceFile: { type: 'string', default: './.tsTrace/trace.json' },
-    typesFile: { type: 'string', default: './.tsTrace/types.json' },
-    pattern: { type: 'string', desc: 'Ruduce the ranking to files that match this glob' },
+    FILE_LIMIT: { type: 'number', default: 50, alias: 'f' },
+    FILE_TYPE_LIMIT: { type: 'number', default: 10, alias: 't' },
+    TRACE_FILE: { type: 'string', default: './.tsTrace/trace.json' },
+    TYPES_FILE: { type: 'string', default: './.tsTrace/types.json' },
+    PATTERN: { type: 'string', desc: 'Ruduce the ranking to files that match this glob', alias: 'p' },
 }).argv;
-traceFile = path_1.resolve(cwd, traceFile);
-typesFile = path_1.resolve(cwd, typesFile);
+TRACE_FILE = path_1.resolve(cwd, TRACE_FILE);
+TYPES_FILE = path_1.resolve(cwd, TYPES_FILE);
+const FILE_LIMIT_CHARS = FILE_LIMIT.toString().length;
+const FILE_TYPE_LIMIT_CHARS = FILE_TYPE_LIMIT.toString().length;
 void (async () => {
-    c.time(':: T');
-    c.log(``);
-    c.group(`ts-rank\n\nRanking output from %o`, `tsconfig.compilerOptions.generateTrace`);
-    c.dir({ logCount, traceFile, typesFile, pattern, cwd });
-    c.log(``);
-    c.time(':: Reading files');
+    console.time(':: T');
+    console.log(``);
+    console.group(`ts-rank\n\nRanking output from %o`, `tsconfig.compilerOptions.generateTrace`);
+    console.dir({ FILE_LIMIT, FILE_TYPE_LIMIT, TRACE_FILE, TYPES_FILE, PATTERN, cwd });
+    console.log(``);
+    console.time(':: Reading files');
     const [trace = [], types = []] = await Promise.all([
-        readJsonFile(traceFile),
-        readJsonFile(typesFile),
+        readJsonFile(TRACE_FILE),
+        readJsonFile(TYPES_FILE),
     ]);
-    c.timeEnd(':: Reading files');
-    c.log(':: %o traces, %o types', trace.length, types.length);
-    const strucTypeMetrics = mapStructuredTypes({ trace, types }).filter(FilterByGlob(pattern));
-    const moduleMetrics = rankMetricsByDur(strucTypeMetrics.filter(({ isNodeModule }) => isNodeModule));
-    const nonModuleMetrics = rankMetricsByDur(strucTypeMetrics.filter(({ isNodeModule }) => !isNodeModule));
-    c.group('\n%o source:', 'StructuredTypeCheck');
-    logMetricGroup(nonModuleMetrics);
-    c.groupEnd();
-    c.group('\n%o node_modules:', 'StructuredTypeCheck');
-    logMetricGroup(moduleMetrics);
-    c.groupEnd();
-    c.timeEnd(':: T');
-    c.groupEnd();
-    c.log('');
+    console.timeEnd(':: Reading files');
+    console.log(':: %o traces, %o types', trace.length, types.length);
+    const checkMetrics = getSymbolCheckMetrics({ trace, types }).filter(FilterByGlob(PATTERN));
+    const metricsMapByPath = Object.entries(lodash_1.groupBy(checkMetrics, 'symbol.firstDeclaration.path'));
+    const sumByMetricDuration = (metrics) => lodash_1.sumBy(metrics, 'check.dur');
+    const orderedMetrics = lodash_1.sortBy(metricsMapByPath, ([path, metrics], compare) => sumByMetricDuration(metrics))
+        .reverse();
+    console.group(`Files ranked by Duration (Wall)`);
+    let fileCount = FILE_LIMIT + 1;
+    orderedMetrics
+        .slice(0, FILE_LIMIT)
+        .reverse()
+        .forEach(([_, metrics]) => {
+        const totalTime = parseFloat((sumByMetricDuration(metrics) / 1000).toFixed(2));
+        --fileCount;
+        console.group(clc.blackBright(`  ${`# ${clc.bold.cyan(fileCount)}`.padEnd(29)} ${clc.bold.red(totalTime)} ms (Total time)`));
+        const typeMetrics = lodash_1.orderBy(metrics, ['check.dur'], 'desc')
+            .slice(0, FILE_TYPE_LIMIT);
+        console.log('');
+        for (const metric of typeMetrics)
+            console.log(`  ${cli.metric(metric)}`);
+        console.log('');
+        console.groupEnd();
+    });
+    console.timeEnd(':: T');
+    console.groupEnd();
+    console.log('');
 })();
-function logMetricGroup(metrics) {
-    let count = 0;
-    for (const metric of metrics)
-        c.log(`${++count}. `.padStart(logCount.toString().length + 2), metricLogMsg(metric));
-    c.log('');
-}
-function metricLogMsg({ symbol: { firstDeclaration: { end, path, start }, symbolName, }, check, }) {
-    const relPath = path_1.relative(cwd, path);
-    const dur = check.dur / 1000; // Get ms
-    return `${dur.toFixed(0).padStart(durChars)} ms < ${symbolName
-        .slice(0, typeChars)
-        .padEnd(typeChars)} > ${relPath}:${start.line}:${start.character}`;
-}
-function mapStructuredTypes({ trace, types }) {
-    const structuredTypeChecks = trace.filter(({ cat, name }) => cat === 'check' && name === 'structuredTypeRelatedTo');
-    const structuredTypeMetrics = [];
+const cli = {
+    count: (v, n = 2) => `${clc.bold(v)}`.padEnd(n.toString().length + 1),
+    time: (v) => clc.bold(v.toFixed(0)),
+    symbolName: (v) => clc.bold.blueBright(v.slice(0, TYPE_CHARS_LEN).padEnd(TYPE_CHARS_LEN)),
+    filePath: (path, pos) => {
+        const dir = path_1.dirname(path);
+        const pathTxt = `${dir + path_1.sep}${clc.underline.bold(path_1.relative(dir, path))}`;
+        return !pos ? pathTxt : pathTxt + clc.blackBright(`:${pos.line}:${pos.character}`);
+    },
+    metric({ symbol: { firstDeclaration: { end, path, start }, symbolName, }, check, }) {
+        const relPath = path_1.relative(cwd, path);
+        const dur = check.dur / 1000; // Get ms
+        return `${(clc.bold.yellow(dur.toFixed(0)) + ' ms').padEnd(27)} ${cli.symbolName(symbolName)} ${cli.filePath(relPath, start)}`;
+    }
+};
+function getSymbolCheckMetrics({ trace, types }) {
+    const validSymbols = ['checkExpression', 'structuredTypeRelatedTo'];
+    const checks = trace.filter(({ cat, name }) => cat === 'check' && validSymbols.includes(name));
+    const metrics = [];
     /** Important for performance */
-    const sourceIdIndexMap = new Map(structuredTypeChecks.map(({ args: { sourceId } }, index) => [sourceId, index]));
+    const sourceIdIndexMap = new Map(checks.map(({ args: { sourceId } }, index) => [sourceId, index]));
     // For loop for perf
     for (const item of types) {
-        if ('symbolName' in item) {
-            // Ignored
-            // TODO: ?? utilize this for reference/alias listing??
-            if (!item.firstDeclaration)
-                continue;
-            const symbol = item;
-            const check = structuredTypeChecks[sourceIdIndexMap.get(symbol.id)];
-            if (!check)
-                continue;
-            structuredTypeMetrics.push({
-                symbol,
-                check,
-                isNodeModule: symbol.firstDeclaration.path.includes('/node_modules'),
-            });
-        }
+        if (!('symbolName' in item))
+            continue;
+        // Ignored
+        // TODO: ?? utilize this for reference/alias listing??
+        if (!item.firstDeclaration)
+            continue;
+        const symbol = item;
+        const check = checks[sourceIdIndexMap.get(symbol.id)];
+        if (!check)
+            continue;
+        metrics.push({
+            symbol,
+            check,
+            isNodeModule: symbol.firstDeclaration.path.includes('/node_modules'),
+        });
     }
-    return structuredTypeMetrics.map((item) => ({
-        ...item,
-        isNodeModule: item.symbol.firstDeclaration.path.includes('/node_modules'),
-    }));
+    return metrics;
 }
 function FilterByGlob(glob) {
     return (metric) => MicroMatch.isMatch(metric.symbol.firstDeclaration.path, glob);
-}
-function rankMetricsByDur(metrics) {
-    return lodash_1.orderBy(metrics, ['check.dur'], 'desc')
-        .slice(0, logCount)
-        .map((item) => ({
-        ...item,
-        isNodeModule: item.symbol.firstDeclaration.path.includes('/node_modules'),
-    }));
 }
 function readFileAsync(pth) {
     return new Promise((resolve) => fs_1.readFile(pth, 'utf8', (_, f) => resolve(f)));
